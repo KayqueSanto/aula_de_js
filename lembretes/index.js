fa6cfe01fbbf2904ce771c341d5e371acca45ce5
KayqueSanto <kayque.a.s@hotmail.com>
2024-04-24 21:17:51 -0300

    atualizações

--- lembretes/index.js
+++ lembretes/index.js
@@ -1,24 +1,39 @@
 const express = require ('express');
+const axios = require("axios");
 const bodyParser = require('body-parser');
 const app = express();
-app.use(bodyParser.json());
-lembretes = {};
-contador = 0;
 
-app.put('/lembretes', (req, res) => {
-    contador++;
+app.use(bodyParser.json())
+let lembretes = {};
+let contador = 0;
+
+app.get('/lembretes', (req, res) => {
+    res.send(lembretes);
+})
+
+app.post("/eventos", (req, res) => {
+    console.log(req.body);
+    res.status(200).send({ msg: "ok" });
+});
+
+app.put('/lembretes', async (req, res) => {
+    contador ++;
     const { texto } = req.body;
+
     lembretes[contador] = {
-        id: contador,
-        texto: texto
+        contador, texto
     };
+
+    await axios.post("http://localhost:10000/eventos", {
+        tipo: "Lembrete Criado",
+        dados: {
+            contador,
+            texto
+        }
+    })
     res.status(201).send(lembretes[contador]);
 });
 
-app.get ('/lembretes', (req, res) => {
-    res.send(lembretes);
-});
-
 app.listen(4000, () => {
-    console.log('lembretes. Porta 4000');
+    console.log('Lembretes Porta 4000');
 });
